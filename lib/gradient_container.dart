8604aad7e6d2b8baac3911691d47ab47055fae24
Samriddhi0214 <167682751+Samriddhi0214@users.noreply.github.com>
2024-07-15 11:28:58 +0530

    Add files via upload

---
+++ lib/gradient_container.dart
@@ -0,0 +1,24 @@
+// import 'package:flutter/material.dart';
+// import 'package:my_app/styled_text.dart';
+
+// const startAlignment = Alignment.topLeft;
+// const endAlignment = Alignment.bottomRight;
+// class GradientContainer extends StatelessWidget{
+//   const GradientContainer(this.colors, {super.key});  
+//   final List<Color> colors;
+//   @override
+//   Widget build(context) {
+//     return Container(
+//       decoration: BoxDecoration(
+//         gradient: LinearGradient(
+//           colors: colors,
+//           begin: startAlignment,
+//           end: endAlignment,
+//         ),
+//       ),
+//       child: Center(
+//         child: StyledText("hola!"),
+//       ),
+//     );
+//   }
+// }
